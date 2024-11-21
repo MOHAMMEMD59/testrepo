fc222d7ed04e3ee820e722351edf5ffd0d9a44c9
mzuser <local@example.com>
2024-11-21 14:48:19 +0100

    ADD TITLE ON BD

--- merchex/listing/views.py
+++ merchex/listing/views.py
@@ -3,8 +3,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from listing.models import Band
+
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont :<p>
+        <ul>
+            <li>{bands[0].name}</li><li>{bands[3].title}</li>
+            <li>{bands[1].name}</li><li>{bands[4].title}</li>
+            <li>{bands[2].name}</li> <li>{bands[5].title}</li>
+        </ul>
+      
+""")
+
 
 def aboutus(request):
      return HttpResponse('<h1>ABOUT US DJANGO TEAM!</h1>')
