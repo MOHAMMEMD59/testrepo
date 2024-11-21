1bfdb06e85469ce7c2e62cdd9a85cadacca41cc0
mzuser <local@example.com>
2024-11-21 15:04:57 +0100

    ADD HTML HELLO PAGE

--- merchex/listing/views.py
+++ merchex/listing/views.py
@@ -5,18 +5,13 @@ from django.shortcuts import render
 
 from listing.models import Band
 
+
+
+
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django !</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li><li>{bands[3].title}</li>
-            <li>{bands[1].name}</li><li>{bands[4].title}</li>
-            <li>{bands[2].name}</li> <li>{bands[5].title}</li>
-        </ul>
-      
-""")
+    return render(request, 'listing/hello.html',
+    {'first_band': bands[0]})
 
 
 def aboutus(request):
