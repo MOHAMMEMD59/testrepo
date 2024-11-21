0f44dc5bd07f2f1d7239e175b2bd4b89aba9b25f
mzuser <local@example.com>
2024-11-21 13:39:33 +0100

    ADD ABOUT US AND CONTACT US PAGE

--- merchex/listing/views.py
+++ merchex/listing/views.py
@@ -1,3 +1,13 @@
+# ~/projects/django-web-app/merchex/listings/views.py
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def aboutus(request):
+     return HttpResponse('<h1>ABOUT US DJANGO TEAM!</h1>')
+
+def contactus(request):
+     return HttpResponse('<h1>Contact us<h1>')
